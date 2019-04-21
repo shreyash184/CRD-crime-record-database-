18c118a17c62273f6c3d145d92417e252e1eb15a
shreyash chavhan <shreyashchavhan8@gmail.com>
2019-04-21 23:42:55 +0530

    Insert query done through frontend

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ con.connect((err) => {
     }
 });
 
-//Functions this function for crimminal_records table
-function mysqlQuery(query, res) {
+//Functions 
+function mysqlQuery(query,  path , res) {
     con.query(query, (err, queryOutput) => {
         if (err) {
             console.log(err);
@@ -29,9 +29,20 @@ function mysqlQuery(query, res) {
         }
         else {
             console.log("query successfully executed");
-            res.render("./showdata/criminalRecords.ejs", { queryOutput: queryOutput });
+            res.render(path, { queryOutput: queryOutput });
         }
     });
+}
+
+//Function for the normal query
+function queries(query, res){
+    con.query(query, (err) => {
+        if(err){
+            console.log(err);
+        } else{
+            console.log("query successfully executed");
+        }
+    })
 }
 
 //Route
@@ -44,7 +55,71 @@ app.get("/query", (req, res) => {
 });
 
 app.get("/criminal-records", (req, res) => {
-    mysqlQuery("SELECT * FROM criminal", res);
+    res.render("./enterdata/criminal.ejs");
+});
+
+app.get("/crime-records", (req, res) => {
+    res.render("./enterdata/crime.ejs")
+});
+
+app.post("/crime-records", (req, res) =>{
+    var pinCode = req.body.pin ;
+    var taluka = req.body.taluka ;
+    var district = req.body.district ;
+    var category = req.body.Catagory ;
+    var timeOfCrime = req.body.Time ;
+    var timeOfReg = req.body.RTime ;
+    var day = req.body.Date ;
+    var month = req.body.month ;
+    var year = req.body.year ;
+    var age = req.body.age ;
+    var query = "INSERT INTO crime_record (case_no, criminal_id, pin_code, village, taluka, district, local_address, time_of_crime, crime_date, registration_time, crime_type) VALUES (24,25,"+pinCode+","+'"wardha"'+","+'"'+taluka+'"'+","+'"'+district+'"'+',"shreyash"'+","+'"'+timeOfCrime+'"'+","+day+","+'"'+timeOfReg+'"'+","+'"'+category+'"'+");";
+    queries(query, res);
+    if(category === "Theft"){
+        res.render("./enterdata/theft.ejs");
+    } else if(category === "Murder"){
+        res.render("./enterdata/murder.ejs");
+    } else if(category === "Railway crime"){
+        res.render("./enterdata/railwayCrime.ejs");
+    } else if(category === "Lost person"){
+        res.render("./enterdata/lostPerson.ejs");
+    } else if(category === "Accident"){
+        res.render("./enterdata/accident.ejs");
+    } else if(category === "Smuggling"){
+        res.render("./enterdata/smuggling.ejs");
+    }
+});
+//Show routes
+app.get("/crime", (req, res) => {
+    mysqlQuery("select * from crime_record", "./showdata/crimeRecords.ejs", res);
+});
+
+app.get("/criminal", (req, res) => {
+    mysqlQuery("select * from criminal", "./showdata/criminalRecords.ejs", res);
+});
+
+app.get("/theft", (req, res) => {
+    mysqlQuery("select * from theft", "./showdata/theft.ejs", res);
+});
+
+app.get("/murder", (req, res) => {
+    mysqlQuery("select * from murder", "./showdata/murder.ejs", res);
+});
+
+app.get("/accident", (req, res) => {
+    mysqlQuery("select * from accident", "./showdata/accident.ej", res);
+});
+
+app.get("/smuggling", (req, res) => {
+    mysqlQuery("select * from smuggling", "./showdata/smuggling.ejs", res);
+});
+
+app.get("/lost", (req, res) => {
+    mysqlQuery("select * from lost_person", "./showdata/lost_person.ejs", res);
+});
+
+app.get("/railway", (req, res) => {
+    mysqlQuery("select * from railway_crime", "./showdata/railway_crimes.ejs", res);
 });
 
 app.post("/query", (req, res) => {
